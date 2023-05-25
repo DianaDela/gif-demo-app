34fcd215ae7774bf2e717c3686a380b2dc43f809
Diana Itzel De la o Veracruz <l19350640@tuxtepec.tecnm.mx>
2023-05-25 12:53:12 -0600

    gif-4

--- src/components/GifsExpo/index.jsx
+++ src/components/GifsExpo/index.jsx
@@ -1,31 +1,55 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const GifsExpo = ({categories}) => {
     const [gifsLists, setGifsLists] = useState([])
 
+    useEffect(
+        () => {
+           const getGifsList = async () => {
 
-    const getGifsList = async () => {
-        let gifArray = []
-        categories.forEach(async(category)=> {
-          const query = encodeURIComponent(category)
-          const apikey = "FDbeafGrIUXzHJYHtvaE1HNPH8MR2Zjx"
-          const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q${query}limit=10`)
-          const gifsLists = await response.json()
+                const gifs = await Promise.all(categories.map(async(category)=> {
+                  const query = encodeURIComponent(category)
+                  const apikey = "FDbeafGrIUXzHJYHtvaE1HNPH8MR2Zjx"
+                  const response = await fetch(
+                    `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${query}&limit=10
+                    `)
+                  const gifsLists = await response.json()
+        
+                 return gifsLists
+               }))
 
-        const urlGifsList = gifsLists.data.map((gif)=> {
-            return gif.images.fixed_width.url
-        })
-        gifArray = [...gifArray, ...urlGifsList]
+               let urlList = [] 
+               console.log(gifs)
 
-       })
-    //    setGifsLists(...gifArray)
+               gifs.forEach((gif) => {
+                const data = gif.data.map((item) =>{
+                    return item.images.fixed_height.url.split('?')[0]
+                })
+                urlList = [...urlList, ...data]
+               })
+               
+               setGifsLists([...urlList])
+            }
 
-    }
-    
-getGifsList()
+            getGifsList()
+            
+        }  
+         ,
+        [categories]
+    )
 
     return (
+        <>
         <h4>GIFSEXPO</h4>
+        <div>
+        {
+            gifsLists.map((url) => (
+                <img key={url} src={url}/>
+            ))
+        }
+        </div>
+        </>
+        
     )
 }
 
